f69e2b797a7ee6fe3fad7ddf9eaef6e873859d2a
Domantas <domantasm96@gmail.com>
2018-05-27 18:48:53 +0300

    Final version

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,31 @@ public class Main {
     private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 
     public static void main(String[] args) throws IOException {
-        encryption("text","key", "enc");
-        decryption("enc", "key", "dec");
-//        if(args.length == 4 ){
-//            if(args[0].equals("-e")){
-//                encryption(args[1], args[2], args[3]);
-//
-//            }
-//            else if(args[0].equals("-d")){
-//                decryption(args[1], args[2], args[3]);
-//            }
-//        }
-//        else{
-//            System.out.println("Arguments not found. \n" +
-//                    "Arguments list: \n" +
-//
-//                    "java Main -e textFile keyFile encryptionFile\n Encryption mode with paths to text file and key file. The last argument is encrypted text filename\n" +
-//                    "java Main -d encryptionFile keyFile outputFile\n Decryption mode with paths to encrypted text file and key file. The last argument is decryption output\n"+
-//                    "----------------------------------------------------------------------------------------\n"+
-//                    "Example:\n"+
-//                    "Encryption: java Main -e text key enc\n"+
-//                    "Decryption: java Main -d enc key output"
-//            );
-//
-//
-//        }
+//        encryption("text","key", "enc");
+//        decryption("enc", "key", "dec");
+        if(args.length == 4 ){
+            if(args[0].equals("-e")){
+                encryption(args[1], args[2], args[3]);
+
+            }
+            else if(args[0].equals("-d")){
+                decryption(args[1], args[2], args[3]);
+            }
+        }
+        else{
+            System.out.println("Arguments not found. \n" +
+                    "Arguments list: \n" +
+
+                    "java Main -e textFile keyFile encryptionFile\n Encryption mode with paths to text file and key file. The last argument is encrypted text filename\n" +
+                    "java Main -d encryptionFile keyFile outputFile\n Decryption mode with paths to encrypted text file and key file. The last argument is decryption output\n"+
+                    "----------------------------------------------------------------------------------------\n"+
+                    "Example:\n"+
+                    "Encryption: java Main -e text key enc\n"+
+                    "Decryption: java Main -d enc key output"
+            );
+
+
+        }
     }
 
     private static void encryption(String textFile, String keyFile, String outputFile){
@@ -53,7 +53,7 @@ public class Main {
 //                encryptFile.write(enc);
 //                encryptFile.close();
 
-                System.out.println("*********** Encryption is completed. Encrypted text is saved in "+outputFile + " file ******************");
+                System.out.println("*********** Encryption is completed. Encrypted text is saved in Encrypted_files/"+outputFile + " file ******************");
             }
             else{
                 System.out.println("Key symbols length should be >= 4\n");
@@ -81,8 +81,8 @@ public class Main {
 //                decryptFile.write(decimalToHex(dec));
 //                decryptFile.close();
 
-                System.out.println("-----------------------DECRYPTION TEXT--------------------------\n\n"+new String(dec)+"\n");
-                System.out.println("*********** "+encryptedFile+" File decryption is done. Encrypted text is saved in "+outputFile + " file ******************");
+                System.out.println("-----------------------DECRYPTED TEXT--------------------------\n\n"+new String(dec)+"\n");
+                System.out.println("*********** "+encryptedFile+" File decryption is done. Encrypted text is saved in Decrypted_files/"+outputFile + " file ******************");
             }
            else{
                 System.out.println("Key symbols length should be >= 4\n");
@@ -92,33 +92,6 @@ public class Main {
             System.out.println("Check if encrypted file or key file exists.");
             return;
         }
-    }
-
-
-
-    private static String decimalToHex(byte[] array){
-        String hex = "";
-        for(int i = 0; i < array.length; i++){
-            hex += Integer.toHexString(array[i]);
-        }
-        return hex;
-    }
-
-    private static String printBytes(byte[] array){
-        String bits = "";
-        for(int i = 0; i < array.length; i++){
-            bits += i != array.length - 1 ? array[i] + " " : array[i];
-        }
-        return bits;
-    }
-
-    private static byte[] stringToBytes(String text){
-        String[] split = text.split(" ");
-        byte[] encryption = new byte[split.length];
-        for(int i = 0; i < split.length; i++){
-            encryption[i] = (byte)Integer.parseInt(split[i]);
-        }
-        return encryption;
     }
 
     private static String bytesToHex(byte[] bytes) {
